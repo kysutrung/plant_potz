667dbd5bc16c5592f080be5e7d86dbde75fb4975
trung_pc_l <trungnd2767@gmail.com>
2024-11-05 14:09:19 +0700

    update

--- scr/testMoistureSen/testMoistureSen/testMoistureSen.ino
+++ scr/testMoistureSen/testMoistureSen/testMoistureSen.ino
@@ -1,9 +1,14 @@
-void setup() {
-  // put your setup code here, to run once:
+int sensorPin = A0;  // Chân analog A0 của ESP8266
+int sensorValue = 0; // Biến lưu trữ giá trị đọc từ cảm biến
 
+void setup() {
+  Serial.begin(115200); // Khởi động Serial với tốc độ 115200
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  sensorValue = analogRead(sensorPin); // Đọc giá trị từ chân analog A0
+  Serial.print("Gia tri analog doc duoc: ");
+  Serial.println(sensorValue);
 
+  delay(1000); // Đọc dữ liệu mỗi giây
 }
