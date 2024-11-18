1cf127af7e5f9c9ef9cc9dd5678c08b26b8ddc90
trung_pc_l <trungnd2767@gmail.com>
2024-11-18 23:04:48 +0700

    no more comment :)))))

--- scr/main001/main001.ino
+++ scr/main001/main001.ino
@@ -7,7 +7,6 @@ Project implementer: Nguyen Lam Thanh Tung, Luong Bao Tuan
 Project developer: Nguyen Duc Trung
 ===========================================================*/
 
-//Thư viện
 #define BLYNK_PRINT Serial
 #define BLYNK_TEMPLATE_ID "TMPL6I10mX5d5"
 #define BLYNK_TEMPLATE_NAME "plant potz"
@@ -15,33 +14,32 @@ Project developer: Nguyen Duc Trung
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
-// Đặt mật khẩu và tên wifi của mình và đây
+
 char ssid[] = "Project 01";
 char pass[] = "vnu.edu.vn";
 
-//Khai báo chân cắm
-/*
-1.28 Round TFT SCREEN
-CS -> D8 (GPIO15)
-DC -> D3 (GPIO0)
-RES -> D4 (GPIO2)
-SDA (MOSI) -> D7 (GPIO13)
-SCL (SCK) -> D5 (GPIO14)
-*/
+
+// 1.28 Round TFT SCREEN
+// CS -> D8 (GPIO15)
+// DC -> D3 (GPIO0)
+// RES -> D4 (GPIO2)
+// SDA (MOSI) -> D7 (GPIO13)
+// SCL (SCK) -> D5 (GPIO14)
+
 #include <TFT_eSPI.h>
 #include <SPI.h>
 TFT_eSPI tft = TFT_eSPI();
 
 #include <DHT.h>
-#define DHTPIN 4         // DHT21 Data PIN -> GPIO4 (D2)
+#define DHTPIN 4   
 #define DHTTYPE DHT21
 DHT dht(DHTPIN, DHTTYPE);
 
-#define SOIL_SENSOR_PIN 0  // Soil moisture sensor DO PIN -> GPIO5 (D1)
+#define SOIL_SENSOR_PIN 0
 
-#define PUMP 5 // 3V water pump VCC PIN -> GPIO5 (D1)
+#define PUMP 5
 
-//Biến toàn cục
+
 bool IS_WATER_PUMP_RUN = 0;
 bool IS_AUTO_MODE_ON = 1;
 float airHumidity = 0;
@@ -156,11 +154,9 @@ void loop() {
   delay(10000);
 }
 
-//Hàm hoạt động với chân ảo Blynk
-
-BLYNK_WRITE(V4)  // Hàm này được gọi khi Virtual Pin V4 thay đổi giá trị
+BLYNK_WRITE(V4)
 {
-    int value_1 = param.asInt();  // Lấy giá trị của V4
+    int value_1 = param.asInt();
     if(value_1 == 0){
       IS_AUTO_MODE_ON = 0;
     }
@@ -169,9 +165,9 @@ BLYNK_WRITE(V4)  // Hàm này được gọi khi Virtual Pin V4 thay đổi giá
     }
 }
 
-BLYNK_WRITE(V5)  // Hàm này được gọi khi Virtual Pin V4 thay đổi giá trị
+BLYNK_WRITE(V5)
 {
-    int value_2 = param.asInt();  // Lấy giá trị của V4
+    int value_2 = param.asInt();
     if(value_2 == 1){
       digitalWrite(PUMP, HIGH);
       IS_WATER_PUMP_RUN = 1;
