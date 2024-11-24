71e36a36cc2ee83e4332cd02df2ae2a3dbd1f726
trung_pc_l <trungnd2767@gmail.com>
2024-11-24 10:37:39 +0700

    switch acc

--- scr/main001/main001.ino
+++ scr/main001/main001.ino
@@ -8,9 +8,14 @@ Project developer: Nguyen Duc Trung
 ===========================================================*/
 
 #define BLYNK_PRINT Serial
-#define BLYNK_TEMPLATE_ID "TMPL6I10mX5d5"
-#define BLYNK_TEMPLATE_NAME "plant potz"
-#define BLYNK_AUTH_TOKEN "zVGuxMgyvz3b5lNrbdO-xHDIjyIqBR9b"
+// #define BLYNK_TEMPLATE_ID "TMPL6I10mX5d5"
+// #define BLYNK_TEMPLATE_NAME "Plant Pot"
+// #define BLYNK_AUTH_TOKEN "zVGuxMgyvz3b5lNrbdO-xHDIjyIqBR9b"
+#define BLYNK_TEMPLATE_ID "TMPL6p4p2SHdQ"
+#define BLYNK_TEMPLATE_NAME "Chau Cay IOT"
+#define BLYNK_AUTH_TOKEN "dnb7zMOT856kDCAtoWHz71vOYjAVRhAn"
+
+
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
